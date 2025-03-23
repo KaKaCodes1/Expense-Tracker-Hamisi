de582a9e0ce3c325c46fe5acbe4c70d1eaafdf1d
Hamisi_Juma <hamisig123@gmail.com>
2025-03-23 23:17:46 +0300

    Commit

--- Backend/obj/Release/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Release/net9.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d139959b97e50621025b3320312a2d4ba34379")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff2b9e47134e65291bb135af62e4973d7be59905")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
